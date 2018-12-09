f299f0711ded252aa08cb042f1a4334e4828c55b
mlasek <michallasek92@gmail.com>
2018-12-09 17:49:17 +0100

    provided some starting dummy movie data for the dummy repository

--- src/main/java/com/mlasek/db/DummyMovieRepository.java
+++ src/main/java/com/mlasek/db/DummyMovieRepository.java
@@ -1,11 +1,28 @@
 package com.mlasek.db;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.mlasek.core.Movie;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.*;
 
 public class DummyMovieRepository implements MovieRepository {
+    public DummyMovieRepository() {
+        try {
+            initializeDummyData();
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    DATA_SOURCE + "missing or unreadable", e);
+        }
+    }
 
+    private static final String DATA_SOURCE = "starting_dummy_data.json";
     private List<Movie> movies = Collections.synchronizedList(new ArrayList<>());
 
     @Override
@@ -36,4 +53,17 @@ public class DummyMovieRepository implements MovieRepository {
     public void delete(UUID id) {
         movies.removeIf(e -> e.getId().equals(id));
     }
+
+    private void initializeDummyData() throws IOException {
+        URL url = Resources.getResource(DATA_SOURCE);
+        String json = Resources.toString(url, Charsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        CollectionType type = mapper
+                .getTypeFactory()
+                .constructCollectionType(List.class, Movie.class);
+        movies = mapper.readValue(json,type);
+    }
+
 }
